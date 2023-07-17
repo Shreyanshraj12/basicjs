bce8a2af2e789dde20253c691ea418ca44b71f46
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2023-07-17 16:55:17 +0530

    Add files via upload

---
+++ Basic JS Project/project1.js
@@ -0,0 +1,6 @@
+function concatenateStrings(arr) {
+    return arr.join('');
+  }
+  var strings = ["Hello", " ", "World"];
+var result = concatenateStrings(strings);
+console.log(result); // Output: "Hello World"
---
+++ Basic JS Project/project2.js
@@ -0,0 +1,12 @@
+function printNumbers(num) {
+    for (var i = 1; i <= num; i++) {
+      console.log(i);
+    }
+  }
+  printNumbers(5);
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
